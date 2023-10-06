3bf410be677e67a03e0ccd5cf478e2c7dde81554
Aleksey <aeksei94@gmail.com>
2023-10-06 17:02:17 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,5 +1,6 @@
-1begin_with_char = "begin_with_char"  # название переменной не должно начинаться с цифры
-one word for var = "one_word_for_var"  # название переменной должно состоять из одного слова
+begin_with_char = "begin_with_char"  # название переменной не должно начинаться с цифры
+one_word_for_var = "one_word_for_var"  # название переменной должно состоять из одного слова
 
-# TODO распечатать содержимое первой переменной
+print(begin_with_char)
 # TODO распечатать содержимое второй переменной
+print(one_word_for_var)
